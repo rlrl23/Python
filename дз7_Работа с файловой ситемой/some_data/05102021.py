30f5f634ded139eaa14a57940a77b87fc8e971a5
Luibov <rlrl23@mail.ru>
2021-10-29 14:00:16 +0500

    Add дз7

---
+++ дз7_Работа с файловой ситемой/some_data/05102021.py
@@ -0,0 +1,123 @@
+# print(speed)
+# speed = 70
+# Ctrl + /
+# import requests
+# pip install requests
+# response = requests.get('http://ya.ru')
+# print(response.content)
+# price = "thousand"
+# print(price)
+# print(id(price))
+# price = 1000
+# print(price)
+# print(id(price))
+# my_var = 'qwe'
+# my_var2 = 'qwe'
+# my_var = ('qwe', 2, 3)
+# my_var2 = ('qwe', 2, 3)
+# print(id(my_var), id(my_var2))
+# print(1 + 1)
+# print(1 - 1)
+# print(2 * 3)
+# print(2 / 3)
+# print(11 % 3) # 3+3+3 +2 =11
+# print(2 ** 3)
+# print(10 / 3)
+# print(10 // 3)
+# print(10 / -3)
+# print(10 // -4)
+# print(5 > 6)
+# print(5 < 6)
+# print(5 == 5)
+# print(5 > "6")
+# print(5 < "6")
+# print(5 == "5")
+# print(5 != 6)
+# print(5 >= 6)
+# print(5 <= 6)
+# print(5 =! 6)
+
+# a = 0
+# b = 5
+# c = -9
+# out = b > a and c < 0
+# print(out)
+
+
+# a = 0
+# b = 5
+# c = 9
+#
+# out = b > a and c < 0
+# # out1 = not((b > a or c < 0) and b == 5)
+# print(out)
+
+# a = 0
+# b = 5
+# c = 9
+#
+# # out = b > a or c < 0
+# out1 = not ((b > a or c < 0) and (b == 5))
+# print(out1)
+
+# user_pass = '1234'
+#
+# if user_pass == input('Введите пароль: '):
+#     print('Авторизован успешно')
+# else:
+#     print('Пароль не верный')
+
+# user_pass = '1234'
+# admin_pass = 'admin'
+# if user_pass == input('Введите пароль: '):
+#     print('Авторизован успешно')
+#     if admin_pass == input('Введите пароль админа: '):
+#         print('rooted')
+#     else:
+#         print('not rooted')
+# else:
+#     print('Пароль не верный')
+
+# if как голова, else как хвост
+# color = 'green'
+# if color == 'green':
+#     print('зеленый')
+# if color == 'red':
+#     print('красный')
+# elif color == 'blue':
+#     print('синий')
+# else:
+#     print('цвет неизвестен')
+
+
+# color = 'green'
+# if color == 'green':
+#     print('зеленый')
+# if color == 'red':
+#     print('красный')
+# if color == 'blue':
+#     print('синий')
+# else:
+#     print('цвет неизвестен')
+#
+# my_list = [100, 'qwe', 540.1, True, [1, 2, 3], {'key': 'value'}]
+# print(my_list[0])
+# my_list.append(100)
+# print(my_list)
+# print(dir(list))
+# print(help(list.sort))
+# my_list = [100, 'qwe', 540.1, True, [1, 2, 3], {'key': 'value'}]
+# print(my_list[::-1])
+
+# nums = [10, 20, 30, 40]
+# indx = 0
+# while indx < len(nums):
+#     print(nums[indx])
+#     indx += 1  # indx = indx + 1
+# my_list = list(range(1, 10, 2))
+# for i in my_list:
+#     print(i)
+#
+# print(my_list)
+
+
