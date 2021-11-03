e67d342bdafc7aa888150ce2e95adf78af5b2999
rlrl23 <90988238+rlrl23@users.noreply.github.com>
2021-11-03 09:03:31 +0500

    Update дз8.py

--- дз8.py
+++ дз8.py
@@ -3,7 +3,7 @@ import re
 
 url = 'someone@geekbrains.ru'
 try:
-    name_domain = re.search(r"(?P<username>[^&]+)(@)(?P<domain>[a-z]+\.[a-z]{2})", url)
+    name_domain = re.search(r"(?P<username>[a-z0-9]+)@(?P<domain>[a-z0-9]+\.[a-z]+)", url)
     print(name_domain.groupdict())
 except AttributeError:
     print('wrong email: ', url)
@@ -28,11 +28,11 @@ def type_logger(func):
     @wraps(func)
     def t_logger(*args, **kwargs):
         result =''
-        for num in list(args):
+        for num in args:
             result += f'calc_cube({num} : {type(num)}, Результат ={func(num)},{type(func(num))}), '
         print(result)
         for key, val in kwargs.items():
-            print(f'calc_cube({str(val)} : {type(val)}, Результат ={func(val)})')
+            print(f'{func.__name__}({str(val)} : {type(val)}, Результат = {func(val)})')
     return t_logger
 
 @type_logger
@@ -42,16 +42,17 @@ def calc_cube(x):
 a = calc_cube(num1=5, num2=23.6)
 b = calc_cube(25.3, 5, 2.0)
 c = calc_cube(1)
+print(calc_cube.__name__)
+print(calc_cube.__doc__)
 
 #4 декоратор, позволяющий валидировать входные значения функции и выбрасывать исключение ValueError
 from functools import wraps
-def val_checker(func):
-    @wraps(func)
-    def v_checker(func_2):
+def val_checker(lam_func):
+    def v_checker(calc_func):
+        @wraps(calc_func)
         def _v_checker(num):
-            result = func_2(num)
-            if func(num):
-                return f'Куб {num} = {result}'
+            if lam_func(num):
+                return f'Куб {num} = {calc_func(num)}'
             else:
                 raise ValueError('ValueError: wrong val', num)
         return _v_checker
